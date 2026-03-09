03e4e8adc0c66c00b09fe15603c931502d1bcd19
tonyreipartsch390-art <tonyrei.partsch390@students.cmcss.net>
2026-03-09 10:42:41 -0500

    y
    g

---
+++ task1.py
@@ -0,0 +1,9 @@
+# Task 1
+torch_lit = True
+
+if torch_lit:
+    outcome = "flicker: the torch lights up and i can see."
+else:
+    outcome = "doom: its dark and i cant see anything."
+
+print(outcome)
